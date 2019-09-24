f515dd84357938384fe4f31ebc419cd7533151e6
Ing William Barbosa <ingeniero.wb@gmail.com>
2019-09-24 12:00:45 -0500

    SE SUBE FUNCIONALIDAD PARA ADICIONAR VARIABLES AL FORMULARIO DE LA PROPUESTAS

--- api/models/Convocatorias.php
+++ api/models/Convocatorias.php
@@ -7,6 +7,11 @@ class Convocatorias extends Model {
     public $id;
 
     public function initialize() {
+        //Se define la relación con 1 a N con Convocatoriaspropuestasparametros
+        $this->hasMany(
+                'id', 'Convocatoriaspropuestasparametros', 'convocatoria'
+        );
+        
         //Se define la relación con 1 a N con Convocatoriasrecursos
         $this->hasMany(
                 'id', 'Convocatoriasrecursos', 'convocatoria'
