3fecdc68f6b71459685b4774b9f53e44d008b7fb
cesar.augusto.britto <cesar.augusto.britto@gmail.com>
2019-09-25 08:18:48 -0500

    Ajustes funcionalidad Hoja de vida jurados

--- api/models/Propuestajuradopublicacion.php
+++ api/models/Propuestajuradopublicacion.php
@@ -7,19 +7,12 @@ class Propuestajuradopublicacion extends Model
 
     public function initialize()
     {
-        //belongsTo	Defines a n-1 relationship
-        $this->hasMany(
-            'usuario_perfil',
-            'Usuariosperfiles',
-            'id'
-        );
-
-        //belongsTo	Defines a n-1 relationship
-        $this->hasMany(
-            'propuesta',
-            'Propuestas',
-            'id'
-        );
+      //belongsTo	Defines a n-1 relationship
+      $this->belongsTo(
+          'propuesta',
+          'Propuestas',
+          'id'
+      );
     }
 
      public function validation(){
