68445197165929762aac4a36c3c81486a5fef906
Ing William Barbosa <ingeniero.wb@gmail.com>
2019-10-23 23:29:04 -0500

    SE INICIA EL DESARROLLO DE CREAR LA PROPUESTA

--- api/controllers/Administrador.php
+++ api/controllers/Administrador.php
@@ -598,6 +598,7 @@ $app->post('/menu', function () use ($app,$config) {
                                 <li><a href="../propuestas/propuestas_busqueda_convocatorias.html">Búsqueda de convocatorias</a></li>
                                 <li><a href="../propuestas/perfiles.html?m=1&id=<?php echo $request->getPost('id');?>">Términos y condiciones de participación</a></li>                                                                
                                 <li><a href="../propuestas/perfil_persona_natural.html?m=<?php echo $request->getPost('m');?>&id=<?php echo $request->getPost('id');?>">Persona natural</a></li>                                
+                                <li><a href="../propuestas/propuestas.html?m=<?php echo $request->getPost('m');?>&id=<?php echo $request->getPost('id');?>">Propuesta</a></li>                                
                                 <?php
                                 }
                                 ?>
